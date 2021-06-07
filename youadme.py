60f477ab742adfbf1fb86cb6ce4820d583c09fa4
priya-tsa <priyaayyori.tsa@gmail.com>
2021-06-07 19:30:14 +0530

    templates

--- youadme.py
+++ youadme.py
@@ -2,35 +2,36 @@ from flask import Flask,render_template,redirect
 app = Flask(__name__)
 
 @app.route('/')
-def index_page():
-    return render_template('index.html')
-
 @app.route('/index')
+def index_page():
+    return render_template('index.html',title='Home')
+
+@app.route('/index1')
 def index_page1():
     return render_template('index1.html')
 
 @app.route('/brand-business')
 def brand_business():
-    return render_template('brand-business.html')
+    return render_template('brand-business.html',title='Brand-Business')
 
 
 @app.route('/consumer')
 def consumer():
-    return render_template('consumer.html')
+    return render_template('consumer.html',title='Consumer')
 
 @app.route('/social-impact')
 def social_impact():
-    return render_template('social-impact.html')
+    return render_template('social-impact.html',title='Social-Impact')
 
 @app.route('/blog')
 def blog():
-    return render_template('blog.html')
+    return render_template('blog.html',title='Blog')
 
 @app.route('/news')
 def news():
-  return render_template('news.html')
+  return render_template('news.html',title='News')
     
 @app.route('/news2')
 def news_page():
-    return render_template('newspage2.html')
+    return render_template('newspage2.html',title='News')
     
